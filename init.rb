e74117505bf54f6bfd016cacb32b0c5b00999ad9
Amaury Balmer <amaury@beapi.fr>
2014-07-15 12:35:19 +0200

    Add initial files for this plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_my_page_feed do
+  name 'MyPage Feed Syndication'
+  author 'Amaury BALMER'
+  description 'Add RSS feed syndication block on my page screen'
+  version '0.1'
+  author_url 'http://www.herewithme.fr'
+end
+
